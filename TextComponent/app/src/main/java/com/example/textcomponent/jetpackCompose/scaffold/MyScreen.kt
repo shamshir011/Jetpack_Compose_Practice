a1f5fa5fe9ae1b5a0d4aa0be27e94e1d3e822c47
Md Shamshir Alam <md.shamshir0001@gmail.com>
2026-03-18 03:17:23 +0530

    Scaffold

--- TextComponent/app/src/main/java/com/example/textcomponent/jetpackCompose/scaffold/MyScreen.kt
+++ TextComponent/app/src/main/java/com/example/textcomponent/jetpackCompose/scaffold/MyScreen.kt
@@ -1,10 +1,18 @@
 package com.example.textcomponent.jetpackCompose.scaffold
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.textcomponent.jetpackCompose.ModifierExample
 
 @Composable
 fun MyScreen(){
@@ -13,9 +21,46 @@ fun MyScreen(){
         topBar = { AppTopBar()},
         bottomBar = {AppBottomBar()},
         floatingActionButton = {FAB()}
-    ) { paddingValues ->
-        Box(modifier = Modifier.padding(paddingValues)) {
-            // Empty for now
+    ) { innerPadding ->
+//        paddingValues ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+
         }
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+
+            for (i in 1..15){
+                MyScreenContent()
+        }
+
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+//            MyScreenContent()
+
+        }
+
+//        LazyColumn(
+//            modifier = Modifier.fillMaxSize()
+//                .padding(innerPadding)
+//                .padding(vertical = 16.dp),
+//            verticalArrangement = Arrangement.spacedBy(10.dp)
+//        ) {
+//            items(15){
+//                MyScreenContent()
+//            }
+//        }
     }
 }
